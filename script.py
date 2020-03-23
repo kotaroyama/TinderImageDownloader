b69527da1b21961d8cb5a193cd15bb56a8bf2428
kotaroyama <kotaro.h.yama@gmail.com>
2020-03-22 17:55:19 -0700

    Add if statement to determine whether the images directory is empty or not; if so, set number_of_images to 0.

--- script.py
+++ script.py
@@ -19,43 +19,61 @@ def getNumOfDownloadedImages():
             index = match.group(0)
             list_of_index.append(int(index))
 
-    number_of_images = max(list_of_index) + 1
+    # Assign the number of images IF the directory is not empty
+    if list_of_index != []:
+        number_of_images = max(list_of_index) + 1
 
     return number_of_images
 
 
 def main():
-    # latitude and longtitude of the location
+    # Latitude and longtitude of the location
+    #   The default location is Times Square:
+    #       LAT = 40.759010
+    #       LON = -73.984474
+
     LAT = 37.786430
     LON = -122.409561
 
-    # your Facebook ID and token
-
+    # Your XAuthToken
+    #   To get your XAuthToken, please refer to:
+    #      "https://github.com/cjekel/tindetheus/issues/7#issuecomment-488878534"
     XAuthToken = 'a02245eb-690c-444d-855e-af3d6e7c02dd'
 
-    # authentication
+    # Authentication
     session = pynder.Session(XAuthToken=XAuthToken)
 
-    # updates latitude and longitude
+    # Update latitude and longitude
     session.update_location(LAT, LON)
 
-    # returns a iterable of users nearby
+    # Return the iterable of users nearby
     users = session.nearby_users()
 
     try:
         print("=============================================================")
+
         for user in users:
+            # Optional: you can choose to swipe like the user
+            # user.like()
+
+            # Print the name and age of the user
             print("Name: " + user.name)
             print("Age:  " + str(user.age))
 
+            # Print the Instagram username
             if user.instagram_username:
                 print("IG:   " + user.instagram_username)
 
+            # List of the Images uploaded by the user
             user_photos = list(user.photos)
 
+            # The number of the images of the user
             number_of_photos = len(user_photos)
+
+            # The number of the images already downloaded
             number_of_downloaded_images = getNumOfDownloadedImages()
 
+            # Ranges to be used in the for loop to download all the images
             range_for_i = range(
                 number_of_downloaded_images,
                 (number_of_downloaded_images + (number_of_photos + 1))
@@ -63,7 +81,7 @@ def main():
 
             range_for_j = range(0, number_of_photos)
 
-            # download images
+            # Download images
             for i, j in zip(range_for_i, range_for_j):
                 filename = "image" + str(i) + ".jpg"
                 urllib.request.urlretrieve(
@@ -71,11 +89,14 @@ def main():
                     "./images/" + filename
                 )
 
-            # show schools
+            # Print the schools
             for school in user.schools:
                 print(school)
 
+            # Print the bio
             print(user.bio)
+
+            # Print the distance between the set locaiton and the user
             print("Distance: " + str(user.distance_km) + "km")
             print(
                 "============================================================="
